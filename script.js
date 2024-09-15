7f56924a918c6768b10189b2d562ba55fd1d6ba7
AbdulHaseeb19862 <prghaseeb518@gmail.com>
2024-09-15 12:13:42 +0500

    task complete

---
+++ script.js
@@ -0,0 +1,19 @@
+const button = document.querySelector("button");
+button.style.backgroundColor = "black";
+button.style.color = "white";
+
+var color = "white";
+
+button.addEventListener("click", () => {
+  if (color === "white") {
+    document.body.style.backgroundColor = "black";
+    button.style.backgroundColor = "white";
+    button.style.color = "black";
+    color = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+    button.style.backgroundColor = "black";
+    button.style.color = "white";
+    color = "white";
+  }
+});
